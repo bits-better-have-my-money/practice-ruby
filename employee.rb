6a151f7cffbe399ab702594af2305d7642f0896e
dani <dani.zaghian@gmail.com>
2021-08-18 11:49:34 -0700

    adds input options hash

--- employee.rb
+++ employee.rb
@@ -17,17 +17,18 @@
 # puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
 # puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
 
+
 # represent an employee as a class instance
 class Employee
   # attr_reader :first_name, :last_name, :salary, :active
   # attr_writer :first_name, :last_name, :active
   attr_accessor :first_name, :last_name, :salary, :active
 
-  def initialize input_first_name, input_last_name, input_salary, input_active
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
   def give_annual_raise
@@ -40,8 +41,8 @@ class Employee
   end
 end
 
-employee1 = Employee.new("Jay", "Wengrow", 20000000, true)
-employee2 = Employee.new("Peter", "Jang", 25000, true)
+employee1 = Employee.new({first_name: "Jay", last_name: "Wengrow", salary: 20000000, active: true})
+employee2 = Employee.new(first_name: "Peter", last_name: "Jang", salary: 25000, active: true)
 employee1.print_info
 employee2.print_info
 puts employee2.give_annual_raise
