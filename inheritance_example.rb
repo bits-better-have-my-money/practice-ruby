325668c50af0e50c126342d109c25e46ebd166ac
dani <dani.zaghian@gmail.com>
2021-08-19 09:57:14 -0700

    adds car and bike specific attributes

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,5 +1,5 @@
 class Vehicle
-  def initialize
+  def initialize(input_options)
     @speed = 0
     @direction = 'north'
   end
@@ -18,19 +18,34 @@ class Vehicle
 end
 
 class Car < Vehicle
+  def initialize(input_options)
+    super
+    @make = input_options[:make]
+    @model = input_options[:model]
+    @color = input_options[:color]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  def initialize(input_options)
+    super
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-car = Car.new
-car.honk_horn
+car = Car.new({make: "Ford", model: "F150", color: "White"})
+p car
+# car.honk_horn
 
-bike = Bike.new
-bike.ring_bell
+bike = Bike.new({type: "Mountain", weight: 25})
+p bike
+# bike.ring_bell
